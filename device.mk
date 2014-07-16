LiquidSmooth-Devices/android_device_asus_grouper

(1/2) Make ROM zip compatible with ext4/f2fs

device.mk
@@ -19,7 +19,25 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 PRODUCT_COPY_FILES := \
     device/asus/grouper/fstab.grouper:root/fstab.grouper \
-    device/asus/grouper/init.grouper.rc:root/init.grouper.rc
+    device/asus/grouper/init.grouper.rc:root/init.grouper.rc \
+    device/asus/grouper/fstab/fstab.grouper.all-EXT4:system/extras/fstab/fstab.grouper.all-EXT4 \
+    device/asus/grouper/fstab/fstab.grouper.all-F2FS:system/extras/fstab/fstab.grouper.all-F2FS \
+    device/asus/grouper/fstab/fstab.grouper.data-F2FS:system/extras/fstab/fstab.grouper.data-F2FS \
+    device/asus/grouper/tools/repack-and-flash.sh:system/extras/tools/repack-and-flash.sh \
+    device/asus/grouper/tools/format-system.sh:system/extras/tools/format-system.sh \
+    device/asus/grouper/tools/kernel/repackimg.sh:system/extras/tools/kernel/repackimg.sh \
+    device/asus/grouper/tools/kernel/unpackimg.sh:system/extras/tools/kernel/unpackimg.sh \
+    device/asus/grouper/tools/kernel/cleanup.sh:system/extras//tools/kernel/cleanup.sh \
+    device/asus/grouper/tools/kernel/authors.txt:system/extras/tools/kernel/authors.txt \
+    device/asus/grouper/tools/kernel/bin/aik:system/extras/tools/kernel/bin/aik \
+    device/asus/grouper/tools/kernel/bin/busybox:system/extras/tools/kernel/bin/busybox \
+    device/asus/grouper/tools/kernel/bin/file:system/extras/tools/kernel/bin/file \
+    device/asus/grouper/tools/kernel/bin/lz4:system/extras/tools/kernel/bin/lz4 \
+    device/asus/grouper/tools/kernel/bin/magic:system/extras/tools/kernel/bin/magic \
+    device/asus/grouper/tools/kernel/bin/mkbootfs:system/extras/tools/kernel/bin/mkbootfs \
+    device/asus/grouper/tools/kernel/bin/mkbootimg:system/extras/tools/kernel/bin/mkbootimg \
+    device/asus/grouper/tools/kernel/bin/unpackbootimg:system/extras/tools/kernel/bin/unpackbootimg \
+    device/asus/grouper/tools/kernel/bin/xz:system/extras/tools/kernel/bin/xz
 
 # the actual meat of the device-specific product definition
 $(call inherit-product, device/asus/grouper/device-common.mk)
@@ -39,11 +57,11 @@ DEVICE_PACKAGE_OVERLAYS := \
 
 # F2FS
 PRODUCT_PACKAGES += \
-    mkfs.f2fs \
-    fsck.f2fs \
-    fibmap.f2fs
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    setup_fs \
-    e2fsck
+    setup_fs \
+    e2fsck

tools/repack-and-flash.sh
@@ -1,0 +1,86 @@
+#!/sbin/sh
+
+# get file descriptor for output (CWM)
+
+OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3);
+
+# get file descriptor for output (TWRP)
+[ $OUTFD != "" ] || OUTFD=$(ps | grep -v "grep" | grep -o -E "updater(.*)" | cut -d " " -f 3)
+
+# functions to send output to recovery
+progress() {
+  if [ $OUTFD != "" ]; then
+    echo "progress ${1} ${2} " 1>&$OUTFD;
+  fi;
+}
+
+set_progress() {
+  if [ $OUTFD != "" ]; then
+    echo "set_progress ${1} " 1>&$OUTFD;
+  fi;
+}
+
+ui_print() {
+  if [ $OUTFD != "" ]; then
+    echo "ui_print ${1} " 1>&$OUTFD;
+    echo "ui_print " 1>&$OUTFD;
+  else
+    echo "${1}";
+  fi;
+}
+
+# make sure all the needed partitions are mounted so they show up in mount
+# this may output errors if the partition is already mounted (/data and /cache probably will be), so pipe them to /dev/null
+# make sure we mount /system before calling any additional shell scripts,
+# because they may use /system/bin/sh instead of /sbin/sh and that may cause problems
+mount /system 2> /dev/null
+mount /cache 2> /dev/null
+mount /data 2> /dev/null
+
+# find out which partitions are formatted as F2FS
+mount | grep -q 'data type f2fs'
+DATA_F2FS=$?
+mount | grep -q 'cache type f2fs'
+CACHE_F2FS=$?
+mount | grep -q 'system type f2fs'
+SYSTEM_F2FS=$?
+
+# unpack the boot.img and subsequently ramdisk via Android Image Kitchen by osm0sis (http://github.com/osm0sis)
+cd /tmp/f2fs/tools/kernel/
+./unpackimg.sh /tmp/f2fs/boot.img
+
+ui_print "Boot.img unpacked."
+set_progress 0.4
+
+# copy the right fstab to the unpacked ramdisk
+ui_print "Setting the right partition layout."
+
+if [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS -eq 0 ] && [ $SYSTEM_F2FS -eq 0 ]; then
+	ui_print "All-F2FS partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.grouper.all-F2FS /tmp/f2fs/tools/kernel/ramdisk/fstab.grouper
+elif [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
+	ui_print "Data-F2FS partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.grouper.data-F2FS /tmp/f2fs/tools/kernel/ramdisk/fstab.grouper
+elif [ $DATA_F2FS != 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
+	ui_print "Standard EXT4 partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.grouper.all-EXT4 /tmp/f2fs/tools/kernel/ramdisk/fstab.grouper
+else
+  ui_print "Unsupported partition layout found."
+  ui_print "No changes were done to the device, exiting."
+  exit
+fi
+set_progress 0.5
+
+# repack the ramdisk and subsequently boot.img via Android Image Kitchen by osm0sis (http://github.com/osm0sis)
+# no parameters are needed because they got saved by the unpacking
+./repackimg.sh
+ui_print "Boot.img repacked."
+set_progress 0.7
+
+# flash the repacked boot.img to the right partition
+# not sure why flash-image doesn't work here, I always get error -1... so I have to dd it, not cool
+# if anyone knows, please tell me :-)
+dd if=/tmp/f2fs/tools/kernel/image-new.img of=/dev/block/platform/sdhci-tegra.3/by-name/LNX
+ui_print "Boot.img flashed."
+set_progress 0.9
+

LiquidSmooth-Devices/android_device_htc_m8

Change-Id: I8c8213cea0a0a66648073d63b3c2dd1595ab2157

device.mk
@@ -20,8 +20,30 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product-if-exists, vendor/htc/m8-common/m8-common-vendor.mk)
 
+
 # overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/m8/overlay
+
+# F2FS tools
+PRODUCT_COPY_FILES := \
+    device/htc/m8/fstab/fstab.qcom.all-EXT4:system/extras/fstab/fstab.qcom.all-EXT4 \
+    device/htc/m8/fstab/fstab.qcom.all-F2FS:system/extras/fstab/fstab.qcom.all-F2FS \
+    device/htc/m8/fstab/fstab.qcom.data-F2FS:system/extras/fstab/fstab.qcom.data-F2FS \
+    device/htc/m8/tools/repack-and-flash.sh:system/extras/tools/repack-and-flash.sh \
+    device/htc/m8/tools/format-system.sh:system/extras/tools/format-system.sh \
+    device/htc/m8/tools/kernel/repackimg.sh:system/extras/tools/kernel/repackimg.sh \
+    device/htc/m8/tools/kernel/unpackimg.sh:system/extras/tools/kernel/unpackimg.sh \
+    device/htc/m8/tools/kernel/cleanup.sh:system/extras//tools/kernel/cleanup.sh \
+    device/htc/m8/tools/kernel/authors.txt:system/extras/tools/kernel/authors.txt \
+    device/htc/m8/tools/kernel/bin/aik:system/extras/tools/kernel/bin/aik \
+    device/htc/m8/tools/kernel/bin/busybox:system/extras/tools/kernel/bin/busybox \
+    device/htc/m8/tools/kernel/bin/file:system/extras/tools/kernel/bin/file \
+    device/htc/m8/tools/kernel/bin/lz4:system/extras/tools/kernel/bin/lz4 \
+    device/htc/m8/tools/kernel/bin/magic:system/extras/tools/kernel/bin/magic \
+    device/htc/m8/tools/kernel/bin/mkbootfs:system/extras/tools/kernel/bin/mkbootfs \
+    device/htc/m8/tools/kernel/bin/mkbootimg:system/extras/tools/kernel/bin/mkbootimg \
+    device/htc/m8/tools/kernel/bin/unpackbootimg:system/extras/tools/kernel/bin/unpackbootimg \
+    device/htc/m8/tools/kernel/bin/xz:system/extras/tools/kernel/bin/xz
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -233,3 +255,14 @@ PRODUCT_COPY_FILES += \
 # OTA ID
 PRODUCT_PROPERTY_OVERRIDES += \
 	otaupdater.otaid=liquidm8
+
+# F2FS
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    setup_fs \
+    e2fsck

tools/repack-and-flash.sh
@@ -1,0 +1,87 @@
+#!/sbin/sh
+
+# get file descriptor for output (CWM)
+
+OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3);
+
+# get file descriptor for output (TWRP)
+[ $OUTFD != "" ] || OUTFD=$(ps | grep -v "grep" | grep -o -E "updater(.*)" | cut -d " " -f 3)
+
+# functions to send output to recovery
+progress() {
+  if [ $OUTFD != "" ]; then
+    echo "progress ${1} ${2} " 1>&$OUTFD;
+  fi;
+}
+
+set_progress() {
+  if [ $OUTFD != "" ]; then
+    echo "set_progress ${1} " 1>&$OUTFD;
+  fi;
+}
+
+ui_print() {
+  if [ $OUTFD != "" ]; then
+    echo "ui_print ${1} " 1>&$OUTFD;
+    echo "ui_print " 1>&$OUTFD;
+  else
+    echo "${1}";
+  fi;
+}
+
+# make sure all the needed partitions are mounted so they show up in mount
+# this may output errors if the partition is already mounted (/data and /cache probably will be), so pipe them to /dev/null
+# make sure we mount /system before calling any additional shell scripts,
+# because they may use /system/bin/sh instead of /sbin/sh and that may cause problems
+mount /system 2> /dev/null
+mount /cache 2> /dev/null
+mount /data 2> /dev/null
+
+# find out which partitions are formatted as F2FS
+mount | grep -q 'data type f2fs'
+DATA_F2FS=$?
+mount | grep -q 'cache type f2fs'
+CACHE_F2FS=$?
+mount | grep -q 'system type f2fs'
+SYSTEM_F2FS=$?
+
+# unpack the boot.img and subsequently ramdisk via Android Image Kitchen by osm0sis (http://github.com/osm0sis)
+cd /tmp/f2fs/tools/kernel/
+./unpackimg.sh /tmp/f2fs/boot.img
+
+ui_print "Boot.img unpacked."
+set_progress 0.4
+
+# copy the right fstab to the unpacked ramdisk
+ui_print "Setting the right partition layout."
+
+if [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS -eq 0 ] && [ $SYSTEM_F2FS -eq 0 ]; then
+	ui_print "All-F2FS partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.qcom.all-F2FS /tmp/f2fs/tools/kernel/ramdisk/fstab.qcom
+elif [ $DATA_F2FS -eq 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
+	ui_print "Data-F2FS partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.qcom.data-F2FS /tmp/f2fs/tools/kernel/ramdisk/fstab.qcom
+elif [ $DATA_F2FS != 0 ] && [ $CACHE_F2FS != 0 ] && [ $SYSTEM_F2FS != 0 ]; then
+	ui_print "Standard EXT4 partition layout found."
+  cp -f /tmp/f2fs/fstab/fstab.qcom.all-EXT4 /tmp/f2fs/tools/kernel/ramdisk/fstab.qcom
+else
+  ui_print "Unsupported partition layout found."
+  ui_print "No changes were done to the device, exiting."
+  exit
+fi
+set_progress 0.5
+
+# repack the ramdisk and subsequently boot.img via Android Image Kitchen by osm0sis (http://github.com/osm0sis)
+# no parameters are needed because they got saved by the unpacking
+./repackimg.sh
+ui_print "Boot.img repacked."
+set_progress 0.7
+
+# flash the repacked boot.img to the right partition
+# not sure why flash-image doesn't work here, I always get error -1... so I have to dd it, not cool
+# if anyone knows, please tell me :-)
+dd if=/tmp/f2fs/tools/kernel/image-new.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
+
+ui_print "Boot.img flashed."
+set_progress 0.9
+